f95a6125d1f21dd77368b418474c435965808ed2
Philip Bogden <pbogdude@gmail.com>
2017-02-06 15:10:40 -0500

    update

--- class-01/bar-chart.js
+++ class-01/bar-chart.js
@@ -1,14 +1,16 @@
+window.onload = ready;
 
-var data = [4, 8, 15, 16, 23, 42];
+function ready() {
+  var data = [4, 8, 15, 16, 23, 42];
 
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, 420]);
+  var x = d3.scale.linear()
+      .domain([0, d3.max(data)])
+      .range([0, 420]);
 
-d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return x(d) + "px"; })
-    .text(function(d) { return d; });
-
+  d3.select(".chart")
+    .selectAll("div")
+      .data(data)
+    .enter().append("div")
+      .style("width", function(d) { return x(d) + "px"; })
+      .text(function(d) { return d; });
+};
